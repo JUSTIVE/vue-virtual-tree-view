6221cc11815f6fa44fe60c41332442ffb2649196
Ben <ben399399@naver.com>
2021-01-23 23:28:15 +0900

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { VueVirtualTreeSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueVirtualTreeSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-virtual-tree-sample />
+  </div>
+</template>
